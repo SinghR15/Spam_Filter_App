72df29a1879c7866278b4b6b24af7b4ac42ccf08
AzwadShameem <shameemazwad@gmail.com>
2022-09-14 10:44:29 -0400

    Directory Modification #2

--- android/app/src/main/kotlin/com/example/spamfilter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.spamfilter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/constants.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-
-String kSpamFilterUrl = "https://spamfilterapi.herokuapp.com";
